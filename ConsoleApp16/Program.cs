af99416c2501bde60c1d47a10d3c514480f7d823
Alexandr Osipov <ossipov44rus@gmail.com>
2022-07-12 19:48:13 +0300

    implemented threading

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -1,28 +1,49 @@
 ﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Text;
 
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+Thread currentThread = Thread.CurrentThread;
+currentThread.Name = "Main";
 var address = $"C:/CodeForces/triplets.txt";
 string[] text = File.ReadAllLines(address, Encoding.UTF8);
 ConcurrentDictionary<string,int> triplets = new ConcurrentDictionary<string, int>();
-var sample = "мамама";
-var sample1 = "папапа";
-Word word = new Word(sample, triplets);
-Word word1 = new Word(sample1, triplets);
-Thread myThread1 = new Thread(word.CountTriplets);
-myThread1.Start();
+foreach (var j in text)
+{
+    var samples = j.Split(' ');
+    int sampleCount = samples.Length;
+    foreach (var k in samples)
+    {
+        Word myWord = new Word(k,triplets);
+        Thread testThread = new Thread(myWord.CountTriplets);
+        testThread.Start();
+        testThread.Join();
+    }
+}
+
+foreach (var i in triplets)
+{
+    Console.WriteLine($"{i.Key}-{i.Value}");
+}
+stopwatch.Stop();
+Console.WriteLine(stopwatch.ElapsedMilliseconds);
 
 
-
-record class Word(string Sample, ConcurrentDictionary<string, int> MyDict)
+record  Word(string Sample, ConcurrentDictionary<string, int> MyDict)
 {
     public void CountTriplets()
     {
-        var counter = Sample.Length - 2;
-        for (int i = 0; i < counter; i++)
+        if (Sample.Length >= 3)
         {
-            StringBuilder triplet = new StringBuilder();
-            triplet.Append(Sample.Substring(i, 3));
-            MyDict.AddOrUpdate(triplet.ToString(), 1, (s, i1) => i1+1 );
+            var counter = Sample.Length - 2;
+            for (int i = 0; i < counter; i++)
+            {
+                StringBuilder triplet = new StringBuilder();
+                triplet.Append(Sample.Substring(i, 3));
+                MyDict.AddOrUpdate(triplet.ToString(), 1, (string s,int i1) => i1+1 );
+            }
         }
+        
     }
 }
