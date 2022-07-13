92775e3d14a04a205ba69d0e4dec427089877541
Alexandr Osipov <ossipov44rus@gmail.com>
2022-07-13 16:18:39 +0300

    timer issues

--- Triplets/Program.cs
+++ Triplets/Program.cs
@@ -2,8 +2,8 @@
 using ConsoleApp16;
 
 var stopwatch = new Stopwatch();
-stopwatch.Start();
 string? address;
+
 /*
 string fileName = "SampleText.txt";
 address = Path.Combine(Directory.GetCurrentDirectory(), fileName);*/
@@ -12,6 +12,7 @@ do
     Console.WriteLine("Введите адрес файла (в виде не пустой строки):");
     address = Console.ReadLine(); 
 } while (address == string.Empty);
+stopwatch.Start();
 TripletCounter.Address = address;
 var triplets = TripletCounter.Triplets;
 TripletCounter tripCount = new TripletCounter();
